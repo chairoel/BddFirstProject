19f2ce70937e7b18cf5da62a0f8c931dd093768f
chairul <chairul.amri@ebconnect.com>
2022-10-14 17:54:48 +0700

    Implicit Intent

--- app/src/main/java/com/test/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/test/myapplication/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.test.myapplication.ui
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -35,7 +36,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             btnThree -> {
-                Toast.makeText(this, btnThree.text.toString(), Toast.LENGTH_SHORT).show()
+                val phoneNumber = "085716163447"
+                startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber")))
             }
 
             btnFour -> {
