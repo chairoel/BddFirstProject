0b8f74aa8dcfe5dfa7f382c0155f62c197110ba4
chairul <chairul.amri@ebconnect.com>
2022-10-17 12:05:43 +0700

    set Recycler view using custom Card View list and change the title according to the selected menu

---
+++ app/src/main/java/com/test/myapplication/adapter/CardViewHeroAdapter.kt
@@ -0,0 +1,75 @@
+package com.test.myapplication.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.test.myapplication.R
+import com.test.myapplication.data.Hero
+
+class CardViewHeroAdapter(
+    private var listHero: ArrayList<Hero>
+) : RecyclerView.Adapter<CardViewHeroAdapter.CardViewViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_cardview_hero, parent, false)
+        return CardViewViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CardViewViewHolder, position: Int) {
+        val context: Context = holder.itemView.context
+        val hero = listHero[position]
+
+        Glide.with(holder.itemView.context)
+            .load(hero.photo)
+            .apply(RequestOptions().override(350, 550))
+            .into(holder.imgPhoto)
+
+        holder.tvName.text = hero.name
+        holder.tvDetail.text = hero.detail
+
+        holder.btnFavorite.setOnClickListener {
+            Toast.makeText(context, "Favorite " + listHero[holder.adapterPosition].name, Toast.LENGTH_SHORT).show()
+        }
+
+        holder.btnShare.setOnClickListener {
+            Toast.makeText(context, "Share " + listHero[holder.adapterPosition].name, Toast.LENGTH_SHORT).show()
+        }
+
+        holder.itemView.setOnClickListener {
+            Toast.makeText(context, "Kamu memilih " + listHero[holder.adapterPosition].name, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listHero.size
+    }
+
+    fun getItem(index: Int): Hero {
+        return listHero[index];
+    }
+
+    fun getItems(): List<Hero?> {
+        return listHero
+    }
+
+    fun setItems(list: List<Hero>) {
+        listHero = list as ArrayList<Hero>
+        notifyDataSetChanged()
+    }
+
+    inner class CardViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imgPhoto: ImageView = itemView.findViewById(R.id.imgItemPhoto)
+        var tvName: TextView = itemView.findViewById(R.id.tvItemName)
+        var tvDetail: TextView = itemView.findViewById(R.id.tvItemDetail)
+        var btnFavorite: Button = itemView.findViewById(R.id.btnSetFavorite)
+        var btnShare: Button = itemView.findViewById(R.id.btnSetShare)
+    }
+}
--- app/src/main/java/com/test/myapplication/ui/RecyclerViewActivity.kt
+++ app/src/main/java/com/test/myapplication/ui/RecyclerViewActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.test.myapplication.R
+import com.test.myapplication.adapter.CardViewHeroAdapter
 import com.test.myapplication.adapter.GridHeroAdapter
 import com.test.myapplication.adapter.ListHeroAdapter
 import com.test.myapplication.data.Hero
@@ -15,13 +16,15 @@ import kotlinx.android.synthetic.main.activity_recycler_view.*
 
 class RecyclerViewActivity : AppCompatActivity() {
 
+    private var title: String = "Recycler View"
     private var list: ArrayList<Hero> = arrayListOf()
     private val listHeroAdapter: ListHeroAdapter = ListHeroAdapter(ArrayList())
     private val gridHeroAdapter: GridHeroAdapter = GridHeroAdapter(ArrayList())
+    private val cardViewHeroAdapter: CardViewHeroAdapter = CardViewHeroAdapter(ArrayList())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
-        supportActionBar?.title = "Recycler View"
+        setActionBarTitle(title)
 
         rvHeroes.setHasFixedSize(true)
         list = HeroesData.listData
@@ -41,16 +44,25 @@ class RecyclerViewActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when (selectedMode) {
             R.id.action_list -> {
+                title = "Mode List"
                 showRecyclerList(list)
             }
 
             R.id.action_grid -> {
+                title = "Mode Grid"
                 showRecyclerGrid(list, 2)
             }
 
             R.id.action_cardview -> {
+                title = "Mode CardView"
+                showRecyclerCardView(list)
             }
         }
+        setActionBarTitle(title)
+    }
+
+    private fun setActionBarTitle(title: String) {
+        supportActionBar?.title = title
     }
 
     private fun showRecyclerList(data: List<Hero>) {
@@ -63,5 +75,11 @@ class RecyclerViewActivity : AppCompatActivity() {
         rvHeroes.layoutManager = GridLayoutManager(this, spanCount)
         rvHeroes.adapter = gridHeroAdapter
         gridHeroAdapter.setItems(data)
+    }
+
+    private fun showRecyclerCardView(data: List<Hero>) {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        rvHeroes.adapter = cardViewHeroAdapter
+        cardViewHeroAdapter.setItems(data)
     }
 }
