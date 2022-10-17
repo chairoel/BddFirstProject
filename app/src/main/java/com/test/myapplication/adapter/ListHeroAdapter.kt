9acb1103b1b85b57ec0b4d2c5c150615b45a295b
chairul <chairul.amri@ebconnect.com>
2022-10-17 11:29:30 +0700

    set Recycler view using LinearLayout list

---
+++ app/src/main/java/com/test/myapplication/adapter/ListHeroAdapter.kt
@@ -0,0 +1,60 @@
+package com.test.myapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.test.myapplication.R
+import com.test.myapplication.data.Hero
+
+
+class ListHeroAdapter(
+    private var listHero: ArrayList<Hero>
+) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val hero = listHero[position]
+
+        Glide.with(holder.itemView.context)
+            .load(hero.photo)
+            .apply(RequestOptions().override(55, 55))
+            .into(holder.imgPhoto)
+
+        holder.tvName.text = hero.name
+
+        holder.tvDetail.text = hero.detail
+    }
+
+    override fun getItemCount(): Int {
+        return listHero.size
+    }
+
+    fun getItem(index: Int): Hero {
+        return listHero[index];
+    }
+
+    fun getItems(): List<Hero?> {
+        return listHero
+    }
+
+    fun setItems(list: List<Hero>) {
+        listHero = list as ArrayList<Hero>
+        notifyDataSetChanged()
+    }
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvName: TextView = itemView.findViewById(R.id.tvItemName)
+        var tvDetail: TextView = itemView.findViewById(R.id.tvItemDetail)
+        var imgPhoto: ImageView = itemView.findViewById(R.id.imgItemPhoto)
+    }
+}
--- app/src/main/java/com/test/myapplication/ui/RecyclerViewActivity.kt
+++ app/src/main/java/com/test/myapplication/ui/RecyclerViewActivity.kt
@@ -2,13 +2,30 @@ package com.test.myapplication.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.test.myapplication.R
+import com.test.myapplication.adapter.ListHeroAdapter
+import com.test.myapplication.data.Hero
+import com.test.myapplication.data.HeroesData
+import kotlinx.android.synthetic.main.activity_recycler_view.*
 
 class RecyclerViewActivity : AppCompatActivity() {
+
+    private var list: ArrayList<Hero> = arrayListOf()
+    private val listHeroAdapter: ListHeroAdapter = ListHeroAdapter(ArrayList())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
         supportActionBar?.title = "Recycler View"
 
+        rvHeroes.setHasFixedSize(true)
+        list = HeroesData.listData
+        showRecyclerList(list)
+    }
+
+    private fun showRecyclerList(data: List<Hero>) {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        rvHeroes.adapter = listHeroAdapter
+        listHeroAdapter.setItems(data)
     }
 }
